fd4960e7ec756683561597d1212219a477122123
yoouzee <99303179+yoouzee@users.noreply.github.com>
2023-03-30 19:48:21 -0400

    changed the jquery code to include the hover function

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,7 @@
 $(document).ready(function() {
-    $('.image').each(function() {
-      var alt = $(this).attr('alt');
-      var img = new Image();
-      img.onload = function() {
-        $(img).data('loaded', true);
-      };
-      img.src = 'images/' + alt + '-large.jpg';
-      $(this).data('large-img', img);
-    }).hover(
-      function() {
-        var $this = $(this);
-        var origSrc = $this.attr('src');
-        var largeImg = $this.data('large-img');
-        if (largeImg && $(largeImg).data('loaded')) {
-          $this.data('orig-src', origSrc);
-          $this.attr('src', largeImg.src);
-        }
-      },
-      function() {
-        var $this = $(this);
-        var origSrc = $this.data('orig-src');
-        if (origSrc) {
-          $this.attr('src', origSrc);
-        }
-      }
-    );
+  $('.image-wrapper').hover(function() {
+      $(this).find('.image-details').show();
+  }, function() {
+      $(this).find('.image-details').hide();
   });
-  
+});
