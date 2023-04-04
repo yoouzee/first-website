d242b9014d72aa23a5fb79b05d176bbbc15b9260
yoouzee <99303179+yoouzee@users.noreply.github.com>
2023-04-04 17:49:04 -0400

    add comments to js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-  $('.image-wrapper').hover(function() {
-      $(this).find('.image-details').show();
+// The following code is a jQuery code that targets a class of elements on a webpage. 
+// It adds an event listener to show or hide a child element when the mouse hovers over or out of the parent element.
+
+ $(document).ready(function() {
+  $('.image').hover(function() { // Attach a hover event listener to each element with the class "image"
+      $(this).siblings('.image-details').show(); // When the mouse hovers over the image element, show the sibling element with the class "image-details"
   }, function() {
-      $(this).find('.image-details').hide();
+      $(this).siblings('.image-details').hide(); // When the mouse moves out of the image element, hide the sibling element with the class "image-details"
   });
-});
+}); 
